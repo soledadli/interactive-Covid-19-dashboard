637a1f52b613ac65ca8fa0142d94cb9c5ca6d245
Dana Almanla <danaalmanla@hotmail.com>
2021-06-04 12:09:29 +0200

    update:app.py for normalized option view

--- app.py
+++ app.py
@@ -21,33 +21,49 @@ def load_data( ):
     df_cases = pd.read_csv('Data_processed/Dataset_COVID_confiremed_complete.csv', index_col = [0])
     df_death= pd.read_csv('Data_processed/Dataset_COVID_Death_complete.csv', index_col=[0])
     df_recovered= pd.read_csv('Data_processed/Dataset_COVID_recovered_complete.csv', index_col=[0])
-    return df_cases,df_death,df_recovered
+    df_population = pd.read_csv('Data_processed/population.csv', index_col=[0])
+    return df_cases,df_death,df_recovered,df_population
 
 '##  Global COVID Cases'
-df_cases,df_death,df_recovered = load_data()
+df_cases,df_death,df_recovered,df_population = load_data()
 country_choice = []
 def df_selection(list_countries_temp, type,df_cases,df_death,df_recovered):
     list_countries_temp[0].append('Date')
     columns_to_filter = list_countries_temp[0]
-    if (type =='death'):
-        print('death')
+    if (type =='Death'):
         return df_death[columns_to_filter]
-    elif (type == 'recovered'):
-        print('recovered')
+    elif (type == 'Recovered'):
         return df_recovered[columns_to_filter]
-    elif (type == 'confirmed'):
-        print('confirmed')
+    elif (type == 'Confirmed'):
         return df_cases[columns_to_filter]
 
-def vis(filter_data,list_countries):
-    print(filter_data)
-    if (len(list_countries[0])<3):
-        fig = px.line(filter_data, x='Date', y=list_countries[0][0])  # 'Daily_France_death')
+def vis(filter_data,list_countries,dt_choice_normal,df_pop):
+    if (dt_choice_normal =='Non-normalized data'):
+        if (len(list_countries[0])<3):
+            fig = px.line(filter_data, x='Date', y=list_countries[0][0])  # 'Daily_France_death')
 
-        return fig
-    elif (len(list_countries[0])>2):
-        fig = px.line(filter_data, x='Date', y=filter_data.columns[0:-1])
-        return fig
+            return fig
+        elif (len(list_countries[0])>2):
+            fig = px.line(filter_data, x='Date', y=filter_data.columns[0:-1])
+            return fig
+    elif(dt_choice_normal=='Normalized over 100k'):
+
+        if (len(list_countries[0]) < 3):
+            df_pop= df_pop.reset_index()
+            population = df_pop[df_pop['Country (or dependency)']==list_countries[0][0]]['Population (2020)']
+            count_1000 = int(population)/100000
+            filter_data[list_countries[0][0]] = filter_data[list_countries[0][0]]/count_1000
+            fig = px.line(filter_data, x='Date', y=list_countries[0][0])  # 'Daily_France_death')
+
+            return fig
+        elif (len(list_countries[0]) > 2):
+            df_pop = df_pop.reset_index()
+            for i in range(0,len(list_countries[0])-1):
+                population = df_pop[df_pop['Country (or dependency)'] == list_countries[0][i]]['Population (2020)']
+                count_1000 = int(population) / 100000
+                filter_data[list_countries[0][i]] = filter_data[list_countries[0][i]]/count_1000
+            fig = px.line(filter_data, x='Date', y=filter_data.columns[0:-1],)
+            return fig
 
 
 # A Sidebar for choosing different functions
@@ -56,7 +72,8 @@ year_2020 = st.sidebar.checkbox('2020')
 year_2021 = st.sidebar.checkbox('2021')
 month = st.sidebar.slider('Month',1,12,6)
 country_choice.append(st.sidebar.multiselect("Choose countries", list(df_cases.columns[0:-3]), default='US'))
-dt_choice = "".join(st.sidebar.multiselect("Choose Category", ['confirmed','death','recovered'],default = 'confirmed'))
+dt_choice = st.sidebar.selectbox("Choose Category", ['Confirmed','Death','Recovered'])
+dt_choice_normal =st.sidebar.selectbox("Choose View", ['Normalized over 100k','Non-normalized data'])
 
 if not country_choice:
     st.sidebar.error("Please select at least one country.")
@@ -64,13 +81,15 @@ if not country_choice:
 if not dt_choice:
     st.sidebar.error("Please select at least one category.")
     #dt_choice = 'confirmed'
+if not dt_choice_normal:
+    st.sidebar.error("Please select number view.")
 
 # Data Viz
 
 
 data_to_plot = df_selection(country_choice, dt_choice,df_cases,df_death,df_recovered)
 
-fig = vis(data_to_plot,country_choice)
+fig = vis(data_to_plot,country_choice,dt_choice_normal,df_population)
 st.plotly_chart(fig)
 
 #if year_2020:
