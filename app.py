12ba368b1b07ad0a8a446aaf516b668a23c18782
Dana Almanla <danaalmanla@hotmail.com>
2021-06-11 12:23:06 +0200

    Add: Map with changing view

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import plotly.express as px
 import datetime
 
 # Setting Dashboard Interface
+st.set_page_config(layout="wide")
 
 st.title('🦠COVID-19 Dashboard')
 st.markdown(
@@ -35,9 +36,10 @@ def load_data( ):
     df_death= pd.read_csv('Data_processed/Dataset_COVID_Death_complete.csv', index_col=[0])
     df_recovered= pd.read_csv('Data_processed/Dataset_COVID_recovered_complete.csv', index_col=[0])
     df_population = pd.read_csv('Data_processed/population.csv', index_col=[0])
-    return df_cases,df_death,df_recovered,df_population
+    df_location = pd.read_csv('time_series_covid19_confirmed_global.csv', index_col=[0])
+    return df_cases,df_death,df_recovered,df_population,df_location
 
-df_cases,df_death,df_recovered,df_population = load_data()
+df_cases,df_death,df_recovered,df_population,df_location = load_data()
 country_choice = []
 
 def df_selection(list_countries_temp, type,df_cases,df_death,df_recovered):
@@ -194,9 +196,57 @@ def vis(filter_data,list_countries,dt_choice_normal, dt_choice_cases, start_date
                     filter_data[new_col] = filter_data.loc[:, i].cumsum()
 
                 fig = px.line(filter_data, x='Date', y=filter_data.columns[len(list_countries[0]) :], 
-                              width=1000, height=500, template= '%s' %(dt_choice_template))
+                              width=1000, height=500, template= '%s' %(dt_choice_template),
+
+                              )
 
                 return fig
+def map_view(df_loca,type,df_cases,df_death,df_recovered,dt_choice_normal, dt_choice_cases, start_date, end_date,df_pop):
+
+    df_loca_sub = df_loca[['Country/Region','Lat','Long']]
+    if (type =='Death'):
+        data_to_work = df_death.copy()
+    elif (type == 'Recovered'):
+        data_to_work = df_recovered.copy()
+    elif (type == 'Confirmed'):
+        data_to_work= df_cases.copy()
+    data_to_work['Date'] = pd.to_datetime(data_to_work['Date'], format='%m/%d/%y')
+    data_to_work = data_to_work.loc[(data_to_work['Date'] >= start_date) & (data_to_work['Date'] <= end_date), :]
+    data_to_work = data_to_work.drop(columns={'Date', 'month', 'year'})
+    data_to_work = data_to_work.sum()
+
+    data_to_work = data_to_work.reset_index().rename(columns={'index': 'Country/Region', 0: 'Total'})
+    data_to_work_full = data_to_work.merge(df_loca_sub, how='left', on='Country/Region')
+
+    df_pop = df_pop.reset_index().rename(columns={'Country (or dependency)': 'Country/Region'})
+    df_pop['count_10000'] = df_pop['Population (2020)'] / 100000
+    data_to_work_full_pop = data_to_work_full.merge(df_pop, how='left', on='Country/Region')
+    data_to_work_full_pop['Normalized data'] = data_to_work_full_pop['Total']/data_to_work_full_pop['count_10000']
+    data_to_work_full_pop = data_to_work_full_pop.drop_duplicates(subset=['Country/Region'], keep='first')
+    data_to_work_full_pop.at[119, 'Lat'] =  48.864716
+    data_to_work_full_pop.at[119, 'Long']= 2.349014
+
+    if (dt_choice_normal=='Non-normalized data'):
+        fig =  px.scatter_mapbox(data_to_work_full_pop, lat="Lat", lon = 'Long',size="Total",  # size of markers, "pop" is one of the columns of gapminder
+                             template='%s' % (dt_choice_template),zoom=0,hover_name = "Country/Region"
+                                ,hover_data = {'Normalized data':True,'Total':True,'Lat' : False,'Long':False})
+        fig.update_layout(mapbox_style="open-street-map",margin=dict(l=20, r=20, t=20, b=20))
+        #fig.update_layout(margin={"r": 0, "l": 0})
+        return fig
+    elif(dt_choice_normal=='Normalized over 100k'):
+        fig = px.scatter_mapbox(data_to_work_full_pop, lat="Lat", lon='Long', size="Normalized data",
+                                # size of markers, "pop" is one of the columns of gapminder
+                                template='%s' % (dt_choice_template), zoom=1,hover_name = "Country/Region"
+                                ,hover_data = {'Normalized data':True,'Total':True,'Lat' : False,'Long':False})
+        fig.update_layout(mapbox_style="open-street-map",margin=dict(l=200, r=20, t=20, b=20),width=1170)
+       # fig.update_layout(margin={"r": 0, "l": 0})
+
+        return fig
+
+
+
+    #fil_data['Cumulative'] = fil_data.iloc[:, 0].cumsum()
+
 
 
 dt_choice_template =st.sidebar.selectbox("Choose Template", ['plotly','ggplot2', 'seaborn', 'simple_white',
@@ -210,7 +260,7 @@ st.sidebar.subheader("Choosing Dates")
 start_date = pd.Timestamp(st.sidebar.date_input('Start date', datetime.date(2020,1,22), min_value=datetime.date(2020,1,22), max_value=datetime.date(2021,5,19)))
 end_date = pd.Timestamp(st.sidebar.date_input('End date', datetime.date(2021,5,20), min_value=datetime.date(2020,1,23), max_value=datetime.date(2021,5,20)))
 
-dt_country = st.multiselect("Choose countries", list(df_cases.columns[0:-3]), default='South Africa')
+dt_country = st.multiselect("Choose countries", list(df_cases.columns[0:-3]), default=['South Africa','Germany'])
 
 country_choice.append(dt_country)
 #country_choice.append(st.sidebar.multiselect("Choose countries", list(df_cases.columns[0:-3]), default='US'))
@@ -218,11 +268,14 @@ if not dt_country:
     st.error("Please select at least one country.")
     st.stop()
 dt_choice = st.sidebar.selectbox("Choose Category", ['Confirmed','Death','Recovered'])
-dt_choice_cases = st.sidebar.selectbox("Choose Case View", ['Daily Cases','7-day rolling', 'Cumulative Cases'])
+dt_choice_cases = st.sidebar.selectbox("Choose Case View", ['7-day rolling','Daily Cases', 'Cumulative Cases'])
 dt_choice_normal =st.sidebar.selectbox("Choose View", ['Normalized over 100k','Non-normalized data'])
 
 
 
+
+fig_map = map_view(df_location,dt_choice,df_cases,df_death,df_recovered,dt_choice_normal, dt_choice_cases, start_date, end_date,df_population)
+st.plotly_chart(fig_map)
 if not dt_choice:
     st.sidebar.error("Please select at least one category.")
     #dt_choice = 'confirmed'
@@ -233,8 +286,9 @@ if not dt_choice_normal:
 
 # Data Viz
 
+
 data_to_plot = df_selection(country_choice, dt_choice,df_cases,df_death,df_recovered)
 #print(data_to_plot.head())
-
 fig = vis(data_to_plot,country_choice,dt_choice_normal,dt_choice_cases, start_date, end_date, df_population)
+fig.update_layout(margin=dict(l=200, r=0, t=20, b=20),width=1300, height=500, )
 st.plotly_chart(fig)
